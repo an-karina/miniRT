492f68286d7c4abce7356f42a13cff73279f1ba9
Jhin Leena <jhleena@ga-o5.msk.21-school.ru>
2021-03-16 16:15:55 +0300

    structers

--- header.h
+++ header.h
@@ -6,38 +6,70 @@
 /*   By: jhleena <jhleena@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/15 20:39:48 by jhleena           #+#    #+#             */
-/*   Updated: 2021/03/15 21:16:26 by jhleena          ###   ########.fr       */
+/*   Updated: 2021/03/16 16:15:39 by jhleena          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma HEADER_H
+#ifndef HEADER_H
+# define HEADER_H
 
-	#include "mlx/mlx.h"
+# include "mlx/mlx.h"
 
-	typedef struct	t_vec
-	{
-		float	x;
-		float	y;
-		float	z;
-	}				s_vec;
+typedef struct	s_vec
+{
+	float		x;
+	float		y;
+	float		z;
+}				t_vec;
+typedef struct	s_point
+{
+	float		x;
+	float		y;
+	float		z;
+}				t_point;
 
-	typedef struct	t_point
-	{
-		float	x;
-		float	y;
-		float	z;
-	}				s_point;
+typedef	struct	s_color
+{
+	float		r;
+	float		g;
+	float		b;
+}				t_color;
 
-	typedef struct t_ray
-	{
-		s_point	point;
-		s_vec	direction;
-	}				s_ray;
+typedef struct	s_ray
+{
+	t_point		point;
+	t_vec		direction;
+}				t_ray;
 
-	typedef struct t_figure
-	{
-		float	sphere_r;
-		float	point;
-	};
-	
-	
+typedef struct s_basis
+{
+	t_vec		i;
+	t_vec		j;
+	t_vec		k;
+}				t_bais;
+
+
+typedef struct	s_camera
+{
+	t_point		position;
+	float		fov;
+	t_vec		view;
+	double		distance;
+	t_bais		camera_base;
+}				t_camera;
+
+typedef struct	s_figure
+{
+	float		s_diameter;
+	t_vec		normal;
+	float		side_size;
+	float		c_height;
+	float		c_diameter;
+	t_point		x_point;
+	t_point		y_point;
+	t_point		z_point;
+	t_color		color;
+	t_camera	camera;
+}				t_figure;
+
+#endif
